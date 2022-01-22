d3de5ad2a157561a049c8b8dd9dff28dceb119c9
PaulSebalu <paolossebalu@gmail.com>
2022-01-22 09:31:40 +0300

    refactor resource location

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `${process.env.API_DOMAIN}`,
+  baseURL: `${process.env.API_DOMAIN || `https://xchenj.herokuapp.com`}`,
   headers: {
     "Content-Type": "application/json",
   },
